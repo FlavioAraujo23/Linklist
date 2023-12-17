0de1903918fbab2038ca27231d6e8251973bb602
FlavioAraujo23 <flaviolopes1027@gmail.com>
2023-12-17 12:26:13 -0300

    :sparkles: feat: Hero section adicionada

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,21 @@
 import Link from 'next/link';
 export default function Header() {
   return (
-    <header className='bg-white border-b flex justify-between p-4'>
-      <div className='flex gap-6'>
-        <Link href={'/'}>LinkList</Link>
-        <nav className='flex items-center gap-4 text-slate-500 text-sm'>
-          <Link href={'/about'}>Sobre</Link>
-          <Link href={'/pricing'}>Preço</Link>
-          <Link href={'/contact'}>Contato</Link>
+    <header className='bg-white border-b py-4'>
+      <div className='max-w-4xl flex justify-between mx-auto px-6'>
+        <div className='flex gap-6'>
+          <Link href={'/'}>LinkList</Link>
+          <nav className='flex items-center gap-4 text-slate-500 text-sm'>
+            <Link href={'/about'}>Sobre</Link>
+            <Link href={'/pricing'}>Preço</Link>
+            <Link href={'/contact'}>Contato</Link>
+          </nav>
+        </div>
+        <nav className='flex gap-4 text-sm text-slate-500'>
+          <Link href={'/login'}>Entrar</Link>
+          <Link href={'/register'}>Cadastrar</Link>
         </nav>
       </div>
-      <nav className='flex gap-4 text-sm text-slate-500'>
-        <Link href={'/login'}>Entrar</Link>
-        <Link href={'/register'}>Cadastrar</Link>
-      </nav>
     </header>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,24 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <section>
+      <section className='p-6 pt-32 max-w-4xl mx-auto'>
+        <div className='max-w-md mb-8'>
+          <h1 className='text-6xl font-bold'>
+            Seu único link <br/> para tudo
+          </h1>
+          <h2 className='text-gray-500 text-xl mt-6'>
+            Compartilhe seus links, redes sociais, informações de contato e mais em uma única página!
+          </h2>
+        </div>
+        <form className='inline-flex items-center shadow-lg shadow-gray-500/20'>
+          <span className='bg-white py-4 pl-4'>
+            linklist.para/
+          </span>
+          <input type="text" className='py-4' placeholder='Nome do usuário'/>
+          <button type="submit" className='bg-blue-500 text-white py-4 px-6'>
+            Inscreva-se de graça
+          </button>
+        </form>
 
       </section>
     </main>
