b1125d844c4958a67fecd00a9c177a9a0753a44b
FlavioAraujo23 <flaviolopes1027@gmail.com>
2023-12-19 15:06:16 -0300

    :sparkles: feat: Adicionei a auth com o google

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,9 @@
-import Link from 'next/link';
-export default function Header() {
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import {getServerSession} from "next-auth";
+import Link from "next/link";
+
+export default async function Header() {
+  const session = await getServerSession(authOptions);
   return (
     <header className='bg-white border-b py-4'>
       <div className='max-w-4xl flex justify-between mx-auto px-6'>
@@ -12,8 +16,20 @@ export default function Header() {
           </nav>
         </div>
         <nav className='flex gap-4 text-sm text-slate-500'>
-          <Link href={'/login'}>Entrar</Link>
-          <Link href={'/register'}>Cadastrar</Link>
+          {!!session && (
+              <>
+                <Link href={'/account'}>
+                  Hello, {session?.user?.name}
+                </Link>
+                
+              </>
+            )}
+            {!session && (
+              <>
+                <Link href={'/login'}>Sign In</Link>
+                <Link href={'/login'}>Create Account</Link>
+              </>
+            )}
         </nav>
       </div>
     </header>
--- src/Components/buttons/LoginWithGoogle.js
+++ src/Components/buttons/LoginWithGoogle.js
@@ -1,11 +1,12 @@
 'use client';
+import {signIn} from "next-auth/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 export default function LoginWithGoogle() {
   return (
     <button
-     onClick={() => {}}
+     onClick={() => {signIn('google')}}
      className="bg-white shadow text-center w-full py-4 flex gap-3 items-center justify-center">
       <FontAwesomeIcon icon={faGoogle} className="h-6"/>
       <span>Entrar com o Google</span>
